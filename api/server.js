4bb84922f763a3ee25acc3186b9357d0ee83c376
Claire List <claire.list2501@gmail.com>
2022-01-18 15:32:07 -0700

    more battles lord

--- api/server.js
+++ api/server.js
@@ -1,7 +1,10 @@
-const express = require("express")
-
-const server = express()
+const express = require("express");
+const server = express();
+const carsRouter = require('./cars/carsRouter');
 
 // DO YOUR MAGIC
+// server.use(express.json());
+
+// server.use('./cars/cars-router', carsRouter); // --> commented out until I build it
 
 module.exports = server
