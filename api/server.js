656746f99a54da537dd600e42d5b4f9d43e6dcb2
Claire List <claire.list2501@gmail.com>
2022-01-18 15:47:01 -0700

    table built in js

--- api/server.js
+++ api/server.js
@@ -3,8 +3,8 @@ const server = express();
 const carsRouter = require('./cars/carsRouter');
 
 // DO YOUR MAGIC
-// server.use(express.json());
+ server.use(express.json());
 
 // server.use('./cars/cars-router', carsRouter); // --> commented out until I build it
 
-module.exports = server
+module.exports = server;
