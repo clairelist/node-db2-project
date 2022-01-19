efd887a1336046d759a109679a9e822bed4d5ad9
Claire List <claire.list2501@gmail.com>
2022-01-18 19:54:47 -0700

    check car id mw built

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,15 +1,36 @@
-// const checkCarId = (req, res, next) => {
-//   // DO YOUR MAGIC
-// }
+//...DATA SECTIONR
+const Car = require('./cars-model');
 
-// const checkCarPayload = (req, res, next) => {
-//   // DO YOUR MAGIC
-// }
+const checkCarId = async (req, res, next) => {
+  // DO YOUR MAGIC --> I am a code witch after all!
+  try{
+    const car = await Car.getById(req.params.id);
+        if(!car){
+            res.status(404).json({ message: `car with id ${req.params.id} is not found` });
+        } else {
+           req.car = car;
+           next();
+        }
+  } catch (err){
+    res.status(500).json({message: err.message})
+  }
+};
 
-// const checkVinNumberValid = (req, res, next) => {
-//   // DO YOUR MAGIC
-// }
+const checkCarPayload = (req, res, next) => {
+  // DO YOUR MAGIC
+}
 
-// const checkVinNumberUnique = (req, res, next) => {
-//   // DO YOUR MAGIC
-// }
+const checkVinNumberValid = (req, res, next) => {
+  // DO YOUR MAGIC
+}
+
+const checkVinNumberUnique = (req, res, next) => {
+  // DO YOUR MAGIC
+}
+
+module.exports = {
+    checkCarId,
+    checkCarPayload,
+    checkVinNumberValid,
+    checkVinNumberUnique
+}
